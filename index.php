5e66c2f15550fda206e7754d1d76e2384069ac8c
Akiras <francesco.scarnera3@gmail.com>
2020-05-20 18:35:36 +0200

    finish milestone 1

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include_once __DIR__ .'/partials/data/data.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="dist/css/main.css">
+</head>
+<body>
+    <header class="Header">
+        <img src="dist/img/spotify-logo.png" alt="">
+    </header>
+    
+    <main class='Main'>
+    
+        <?php foreach( $database as $data ) { ?>
+            <div class="cd">
+                <img src="<?php echo $data['cover'];?>" alt="">
+                <h3><?php echo $data['album'];?></h3> 
+                <span class="grey"><?php echo $data['author'];?></span>
+                <span><?php echo $data['year'];?></span>
+            </div>
+        <?php } ?>
+    </main>
+</body>
+</html>
---
+++ partials/data/data.php
@@ -0,0 +1,87 @@
+<?php
+$database = [
+    [
+        'cover' => 'dist/img/hendrix.jpg',
+        'album' => 'Axis: Bold As Love',
+        'author' => 'The Jimi Hendrix Experience',
+        'year' => '1967'
+    ],
+    [
+        'cover' => 'dist/img/the.rolling.s.jpg',
+        'album' => 'Some Girls',
+        'author' => 'The Rolling Stones',
+        'year' => '1978',
+    ],
+    [
+        'cover' => 'dist/img/springsteen.jpg',
+        'album' => 'Born in the U.S.A.',
+        'author' => 'Bruce Springsteen',
+        'year' => '1984',
+    ],
+    [
+        'cover' => 'dist/img/the.velvet.u.jpg',
+        'album' => 'The Velvet Underground & Nico',
+        'author' => 'Velvet Underground',
+        'year' => '1967',
+    ],
+    [
+        'cover' => 'dist/img/grace.jones.jpg',
+        'album' => 'Slave To The Rythm',
+        'author' => 'Grace Jones',
+        'year' => '1985'
+    ],
+    [
+        'cover' => 'dist/img/pink.jpg',
+        'album' => 'The Dark Side of the Moon',
+        'author' => 'Pink Floyd ',
+        'year' => '1973',
+    ],
+    [
+        'cover' => 'dist/img/david.bowie.jpg',
+        'album' => 'Aladdin Sane',
+        'author' => 'David Bowie',
+        'year' => '1973'
+    ],
+    [
+        'cover' => 'dist/img/santana.jpg',    
+        'album' => 'Abraxas',
+        'author' => 'Santana',
+        'year' => '1970',
+    ],
+    [
+        'cover' => 'dist/img/the.beatles.jpg',
+        'album' => 'Sgt. Pepper’s Lonely Hearts Club Band',
+        'author' => 'The Beatles',
+        'year' => '1967',
+    ],
+    [
+        'cover' => 'dist/img/the.roling.stone.2.jpg',
+        'album' => 'Exile On Main St.',
+        'author' => 'The Rolling Stones',
+        'year' => '1972',
+    ],
+    [
+        'cover' => 'dist/img/bob.dylan.jpg',
+        'album' => 'Self Portrait',
+        'author' => 'Bob Dylan',
+        'year' => '1970',
+    ],
+    [
+        'cover' => 'dist/img/janes.addiction.jpg',
+        'album' => 'Nothing’s Shocking',
+        'author' => 'Jane’s Addiction ',
+        'year' => '1988',
+    ],
+    [
+        'cover' => 'dist/img/king.crimson.jpg',
+        'album' => 'In The Court Of The Crimson King (An Observation By King Crimson)',
+        'author' => 'King Crimson',
+        'year' => '1969',
+    ],
+    [
+        'cover' => 'dist/img/pink.floyd.2.jpg',
+        'album' => 'Wish You Were Here',
+        'author' => 'Pink Floyd',
+        'year' => '1975',
+    ],
+];
